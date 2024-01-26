feefdda99cce06cf434b531690195384c11c87bd
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-01-26 13:22:16 -0300

    refactor: moves business logic to use case

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,7 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
-import { hash } from 'bcryptjs'
 import { z } from 'zod'
 
-import { prisma } from '@/lib'
+import { registerUseCase } from '@/usecases'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -13,16 +12,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
-  const passwordHash = await hash(password, 6)
-
-  const userWithSameEmail = await prisma.user.findUnique({ where: { email } })
-  if (userWithSameEmail) {
+  try {
+    await registerUseCase({ name, email, password })
+  } catch (error) {
     return reply.status(409).send()
   }
-
-  await prisma.user.create({
-    data: { name, email, passwordHash },
-  })
 
   return reply.status(201).send()
 }
