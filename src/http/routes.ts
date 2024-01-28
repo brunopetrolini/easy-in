0fb37170b79a110f27d378ecb849f746b418ebe7
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-01-28 20:38:42 -0300

    feat: adds sessions route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,7 +1,8 @@
 import type { FastifyInstance } from 'fastify'
 
-import { register } from './controllers'
+import { authenticate, register } from './controllers'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
+  app.post('/sessions', authenticate)
 }
