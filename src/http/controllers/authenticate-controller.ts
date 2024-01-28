b536609c1d9e15859e82474a9a3925e1d70250e4
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-01-28 20:36:52 -0300

    feat: adds authenticate controller

---
+++ src/http/controllers/authenticate-controller.ts
@@ -0,0 +1,32 @@
+import type { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { PrismaUsersRepository } from '@/repositories'
+import { AuthenticateUseCase } from '@/usecases'
+import { InvalidCredentialsError } from '@/usecases/error'
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, password } = authenticateBodySchema.parse(request.body)
+
+  try {
+    const usersRepository = new PrismaUsersRepository()
+    const authenticateUseCase = new AuthenticateUseCase(usersRepository)
+
+    await authenticateUseCase.execute({ email, password })
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(401).send({ message: error.message })
+    }
+    throw error
+  }
+
+  return reply.status(200).send()
+}
