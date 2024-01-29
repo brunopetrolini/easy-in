02177ec1b8a1f26908092bf7c24d72624404d3b3
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-01-28 21:37:16 -0300

    feat: adds `findById` method in users repository

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -7,7 +7,7 @@ import type { UsersRepository } from '../users-repository'
 export class InMemoryUsersRepository implements UsersRepository {
   public readonly users: User[] = []
 
-  public async insert(data: Prisma.UserCreateInput) {
+  public async insert(data: Prisma.UserCreateInput): Promise<User> {
     this.users.push({
       id: randomUUID(),
       name: data.name,
@@ -19,8 +19,18 @@ export class InMemoryUsersRepository implements UsersRepository {
     return this.users[this.users.length - 1]
   }
 
-  public async findByEmail(email: string) {
+  public async findByEmail(email: string): Promise<User | null> {
     const user = this.users.find((user) => user.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
+  public async findById(id: string): Promise<User | null> {
+    const user = this.users.find((user) => user.id === id)
 
     if (!user) {
       return null
--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,16 +1,21 @@
-import type { Prisma } from '@prisma/client'
+import type { Prisma, User } from '@prisma/client'
 
-import { prisma } from '@/lib'
 import type { UsersRepository } from '../users-repository'
+import { prisma } from '@/lib'
 
 export class PrismaUsersRepository implements UsersRepository {
-  public async insert(data: Prisma.UserCreateInput) {
+  public async insert(data: Prisma.UserCreateInput): Promise<User> {
     const user = await prisma.user.create({ data })
     return user
   }
 
-  public async findByEmail(email: string) {
+  public async findByEmail(email: string): Promise<User | null> {
     const user = await prisma.user.findUnique({ where: { email } })
+    return user
+  }
+
+  public async findById(id: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({ where: { id } })
     return user
   }
 }
