0081a73a5ee08851d2159412c12d582b1b7d05d6
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-02-04 21:01:28 -0300

    test: ensure to able to register a new user

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,24 @@
-import { expect, test } from 'vitest'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {
-  expect(1).toBe(1)
+import { app } from '@/app'
+
+describe('Register [E2E]', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@mail.com',
+      password: 'JohnDoe123',
+    })
+
+    expect(response.statusCode).toBe(201)
+  })
 })
