20b8e90831711dd8394120057f6ace753089eba5
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-02-18 20:55:11 -0300

    feat: adds fetch nearby gyms controller and route

---
+++ src/http/controllers/gyms/nearby-gyms-controller.ts
@@ -0,0 +1,22 @@
+import type { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { makeFetchNearbyGymUseCase } from '@/usecases/factories'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymQuerySchema = z.object({
+    latitude: z.number().refine((value) => Math.abs(value) <= 90),
+    longitude: z.number().refine((value) => Math.abs(value) <= 180),
+  })
+
+  const { latitude, longitude } = nearbyGymQuerySchema.parse(request.body)
+
+  const nearbyGymsUseCase = makeFetchNearbyGymUseCase()
+
+  const gyms = await nearbyGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(200).send({ gyms })
+}
--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -3,10 +3,12 @@ import { FastifyInstance } from 'fastify'
 import { verifyAuthMiddy } from '@/http/middlewares'
 import { create } from './create-gym-controller'
 import { search } from './search-gyms-controller'
+import { nearby } from './nearby-gyms-controller'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyAuthMiddy)
 
   app.post('/gyms', create)
   app.get('/gyms', search)
+  app.get('/gyms', nearby)
 }
