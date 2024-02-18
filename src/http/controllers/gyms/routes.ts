4293b3e75a878d428a1aeb5c0bc894a819475e9a
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-02-18 20:48:49 -0300

    feat: adds search gyms controller and route

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -2,9 +2,11 @@ import { FastifyInstance } from 'fastify'
 
 import { verifyAuthMiddy } from '@/http/middlewares'
 import { create } from './create-gym-controller'
+import { search } from './search-gyms-controller'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyAuthMiddy)
 
   app.post('/gyms', create)
+  app.get('/gyms', search)
 }
---
+++ src/http/controllers/gyms/search-gyms-controller.ts
@@ -0,0 +1,19 @@
+import type { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { makeSearchGymsUseCase } from '@/usecases/factories'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymQuerySchema = z.object({
+    query: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { query, page } = searchGymQuerySchema.parse(request.body)
+
+  const searchGymsUseCase = makeSearchGymsUseCase()
+
+  const gyms = await searchGymsUseCase.execute({ query, page })
+
+  return reply.status(200).send({ gyms })
+}
