0d3a84eb96039a3998603fc43706a7638fe168cc
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-02-18 21:11:32 -0300

    refactor: simplify methods name

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,9 +1,9 @@
 import { FastifyInstance } from 'fastify'
 
 import { verifyAuthMiddy } from '@/http/middlewares'
-import { create } from './create-gym-controller'
-import { search } from './search-gyms-controller'
-import { nearby } from './nearby-gyms-controller'
+import { create } from './create-controller'
+import { search } from './search-controller'
+import { nearby } from './nearby-controller'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyAuthMiddy)
