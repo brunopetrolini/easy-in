a143e2179efa36566c0045b5c010b8e7466c2e89
Bruno Petrolini <bruno.petrolini@contasimples.com>
2024-03-19 20:34:50 -0300

    test: adds refresh token route tests

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -4,10 +4,12 @@ import { verifyAuthMiddy } from '@/http/middlewares'
 import { register } from './register-controller'
 import { profile } from './profile-controller'
 import { authenticate } from './authenticate-controller'
+import { refresh } from './refresh-controller'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/sessions', authenticate)
   app.post('/users', register)
+  app.patch('/token/refresh', refresh)
 
   /** Authenticated */
   app.get('/me', { onRequest: [verifyAuthMiddy] }, profile)
